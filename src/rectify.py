c671d9cf8e414b7bebf596364793253a9935765e
Frank Palazzolo <frank@avoidspikes.com>
2025-11-15 21:07:20 -0500

    Default window is bigger

--- src/rectify.py
+++ src/rectify.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
     window = MainWindow()
-    #window.setGeometry(500, 300, 800, 600)
+    window.setGeometry(500, 300, 800, 600)
     window.show()
     sys.exit(app.exec())
